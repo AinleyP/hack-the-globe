12187aff431de2ef1ced8f5986cba1d8322d3dc6
daniel-sm-yu <daniel.sm.yu@gmail.com>
2021-03-13 15:59:16 -0500

    create Resource enum

--- src/data/types/responsiblity.ts
+++ src/data/types/responsiblity.ts
@@ -1,10 +1,13 @@
 export default interface Responsiblity {
-  name:
-    | 'Provide Funding'
-    | 'Contact Local Police'
-    | 'Outreach'
-    | 'Sound Equipment'
-    | 'Safety Training'
-    | 'Safety Personnel';
+  name: Resource;
   status: 'filled' | 'unfilled';
+}
+
+export enum Resource {
+  funding = 'Provide Funding',
+  police = 'Contact Local Police',
+  outreach = 'Outreach',
+  sound = 'Sound Equipment',
+  training = 'Safety Training',
+  personnel = 'Safety Personnel',
 }
