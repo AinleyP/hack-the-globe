b6aa99b1760be2f3fd7ca1208f70ace9cef6aadd
daniel-sm-yu <daniel.sm.yu@gmail.com>
2021-03-13 17:27:22 -0500

    add events to sample activist

--- src/data/sample_data/activist.ts
+++ src/data/sample_data/activist.ts
@@ -1,9 +1,10 @@
-import Activist from '../types/activist'
+import Activist from '../types/activist';
 
 const activist: Activist = {
   id: 1,
-  name: "Jane Doe",
-  image: "https://source.unsplash.com/plsF6obTgms/150x150"
-}
+  name: 'Jane Doe',
+  image: 'https://source.unsplash.com/plsF6obTgms/150x150',
+  events: [],
+};
 
-export default activist
+export default activist;
