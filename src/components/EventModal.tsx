b5e8ff423f8221a7882df8dc250d85a1a285dcc7
Megan Niu <megan.niu@gmail.com>
2021-03-14 10:05:43 -0400

    Add edit tags field

--- src/components/EventModal.tsx
+++ src/components/EventModal.tsx
@@ -1,29 +1,67 @@
 /* Imports from packages */
-import { connect } from "react-redux";
-import { useLocation } from 'react-router-dom'
+import { bindActionCreators, Dispatch } from "redux";
+import { connect, ConnectedProps } from "react-redux";
+import { useState } from "react";
 
+import { editEvent } from '../data/actions/activistActions'
 import Event from '../data/types/event'
+import Responsibility, { Resource, stringToResource } from '../data/types/responsiblity'
 import { RootState } from '../data/reducers'
 import CloseIcon from '../assets/close-icon.svg'
+import PenIcon from '../assets/pen.svg'
 import SupportingTags from '../pages/SupportingTags'
 import Tags from "./Tags";
+import TagsInput from 'react-tagsinput'
+import 'react-tagsinput/react-tagsinput.css' // If using WebPack and style-loader.
 
-interface Props {
-  setModalVisibility: (isOpen: boolean) => void,
-  event: Event
-};
+interface DispatchProps {
+  editEvent: typeof editEvent,
+}
+
+//create your forceUpdate hook
+function useForceUpdate(){
+  const [value, setValue] = useState(0); // integer state
+  return () => setValue(value => value + 1); // update the state to force render
+}
 
 const EventModal = (props: Props): JSX.Element => {
-  const location = useLocation();
+  const [isEditing, setIsEditing] = useState(false)
+  const [event, setEvent] = useState(props.event)
+  const forceUpdate = useForceUpdate();
 
   const onClickViewDetails = () => {
     props.setModalVisibility(false)
   }
 
-  console.log(location.pathname);
+  const onTagsChange = (newResources: Array<string>, changedResources: Array<Resource>) => {
+    let eventResponsibilities: Array<Responsibility> = event.responsiblities.map((resp) => resp)
+    event.responsiblities.length = 0
 
-  const isCurrentPath = (regex: RegExp): boolean => regex.test(location.pathname)
+    newResources.forEach((resName: string) => {
+      let found = false;
+      const res = stringToResource(resName)
 
+      eventResponsibilities.forEach((resp: Responsibility) => {
+        if (resp.name == res) {
+          found = true;
+          event.responsiblities.push(resp)
+        }
+      })
+
+      if (!found) {
+        event.responsiblities.push({
+          name: res,
+          status: 'unfilled'
+        })
+      }
+    })
+
+    console.log(event)
+    setEvent(event)
+    props.editEvent(event)
+
+    forceUpdate()
+  }
 
   return <div>
     <div className="overlay"></div>
@@ -33,24 +71,31 @@ const EventModal = (props: Props): JSX.Element => {
       </div>
       <div className="event-modal-content">
         <div className="event-modal-title">
-          <img src={props.event.image} />
+          <img src={event.image} />
           <div className="event-modal-title-section header-text">
-            <h1>{props.event.name}</h1>
+            <h1>{event.name}</h1>
             <div className="event-modal-supporting-tags">
-              <SupportingTags supportingTags={props.event.supportingTags ? props.event.supportingTags : []} />
+              <SupportingTags supportingTags={event.supportingTags ? event.supportingTags : []} />
             </div>
             <h3>Volunteers Needed</h3>
             <h3>Host: Jane Doe</h3>
             <h3>Event Site: facebook.com/protest-event</h3>
-            <h3>Location: Toronto, Ontario, Canada</h3>
+            <h3>Location: San Francisco, California, U.S.</h3>
           </div>
         </div>
         <div className="event-modal-text">
           <div className="event-modal-title-section">
-            <h2>Resources required</h2>
+            <div className="event-modal-resources-required">
+              <h2>Resources required</h2>
+              <button onClick={() => { setIsEditing(!isEditing) }}>
+                {isEditing ? "Save" : <img src={PenIcon} />}
+              </button>
+            </div>
           </div>
-          <Tags tags={props.event.responsiblities.map((item) => item.name)} />
-
+          {isEditing ?
+            <TagsInput value={event.responsiblities.map((item) => item.name)} onChange={onTagsChange} />
+            : <Tags tags={event.responsiblities.map((item) => item.name)} />
+          }
           <div className="event-modal-title-section">
             <h2>Objectives</h2>
           </div>
@@ -59,7 +104,7 @@ const EventModal = (props: Props): JSX.Element => {
           <div className="event-modal-title-section">
             <h2>Plan</h2>
           </div>
-          <p>{props.event.description}</p>
+          <p>{event.description}</p>
 
           {/* <div className="modal-footer"> */}
           {/* <button type="button" className="organization-card-button" data-dismiss="modal">Close</button> */}
@@ -71,4 +116,21 @@ const EventModal = (props: Props): JSX.Element => {
   </div>;
 };
 
-export default EventModal;
+const mapDispatchToProps = (dispatch: Dispatch): DispatchProps => {
+  return bindActionCreators(
+    {
+      // add other actions here
+      editEvent
+    },
+    dispatch
+  );
+};
+
+const connector = connect(null, mapDispatchToProps);
+
+type Props = ConnectedProps<typeof connector> & {
+  setModalVisibility: (isOpen: boolean) => void,
+  event: Event,
+};
+
+export default connector(EventModal);
--- src/data/reducers/activistReducer.ts
+++ src/data/reducers/activistReducer.ts
@@ -1,7 +1,9 @@
 /* Imports from local files */
 import Activist from '../types/activist'
+import Event from '../types/event'
 import { ActivistAction } from '../actions/'
 import sampleActivist from '../sample_data/activist'
+import { EDIT_EVENT } from '../actions/actionTypes'
 
 export interface ActivistState {
   data: Activist
@@ -16,7 +18,22 @@ const defaultState: ActivistState = {
  * these replacements should be immutable.
  */
 export default (state = defaultState, action: ActivistAction): ActivistState => {
+  let stateCopy = state
+
   switch (action.type) {
+
+    case EDIT_EVENT:
+      const newEvents = []
+      
+      stateCopy.data.events.forEach((event: Event) => {
+        if(event.id !== (action.payload as Event).id){
+          newEvents.push(event)
+        }
+      })
+
+      newEvents.unshift(action.payload as Event)
+      stateCopy.data.events = newEvents
+      return stateCopy
     default:
       return state;
   }
