09daa03e2d4d59c5b0469435f8c47b13e09323e4
daniel-sm-yu <daniel.sm.yu@gmail.com>
2021-03-14 09:50:04 -0400

    added city to event

--- src/components/ActivistProfileEventCard.tsx
+++ src/components/ActivistProfileEventCard.tsx
@@ -1,21 +1,31 @@
 /* Imports from packages */
-import Event from '../data/types/event'
-import Responsibility, { Resource } from '../data/types/responsiblity'
-import Tags from './Tags'
+import Event from '../data/types/event';
+import Responsibility, { Resource } from '../data/types/responsiblity';
+import Tags from './Tags';
 
 interface Props {
-  event: Event
+  event: Event;
 }
 
 const ActivistProfileEventCard = (props: Props): JSX.Element => {
-  return <div className="profile-event-card">
-    <img src={props.event.image} />
-    <div className="profile-event-card-text">
-      <h1>{props.event.name}</h1>
-      <p>{props.event.description ? `${props.event.description.substring(0, 100)}...` : ""}</p>
-      <Tags tags={props.event.responsiblities ? props.event.responsiblities.map((responsibility: Responsibility) => responsibility.name) : []} />
+  return (
+    <div className="profile-event-card">
+      <img src={props.event.image} />
+      <div className="profile-event-card-text">
+        <h1>{props.event.name}</h1>
+        <p>
+          {props.event.description ? `${props.event.description.substring(0, 100)}... ${props.event.location}` : ''}
+        </p>
+        <Tags
+          tags={
+            props.event.responsiblities
+              ? props.event.responsiblities.map((responsibility: Responsibility) => responsibility.name)
+              : []
+          }
+        />
+      </div>
     </div>
-  </div>;
+  );
 };
 
-export default ActivistProfileEventCard
+export default ActivistProfileEventCard;
