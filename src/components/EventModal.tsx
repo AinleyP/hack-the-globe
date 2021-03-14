a2f14c9bb5cc9e2ed0260ec360b3be920a420029
daniel-sm-yu <daniel.sm.yu@gmail.com>
2021-03-14 10:30:14 -0400

    make model suggest

--- src/components/EventModal.tsx
+++ src/components/EventModal.tsx
@@ -1,27 +1,32 @@
 /* Imports from packages */
-import { bindActionCreators, Dispatch } from "redux";
-import { connect, ConnectedProps } from "react-redux";
-import { useState } from "react";
+import { bindActionCreators, Dispatch } from 'redux';
+import { connect, ConnectedProps } from 'react-redux';
+import { useState } from 'react';
+import { bindActionCreators, Dispatch } from 'redux';
+import { connect, ConnectedProps } from 'react-redux';
 
-import { editEvent } from '../data/actions/activistActions'
-import Event from '../data/types/event'
-import Responsibility, { Resource, stringToResource } from '../data/types/responsiblity'
-import { RootState } from '../data/reducers'
-import CloseIcon from '../assets/close-icon.svg'
-import PenIcon from '../assets/pen.svg'
-import SupportingTags from '../pages/SupportingTags'
-import Tags from "./Tags";
-import TagsInput from 'react-tagsinput'
-import 'react-tagsinput/react-tagsinput.css' // If using WebPack and style-loader.
+import { editEvent } from '../data/actions/activistActions';
+import Event from '../data/types/event';
+import Responsibility, { Resource, stringToResource } from '../data/types/responsiblity';
+import { RootState } from '../data/reducers';
+import CloseIcon from '../assets/close-icon.svg';
+import PenIcon from '../assets/pen.svg';
+import SupportingTags from '../pages/SupportingTags';
+import Tags from './Tags';
+import TagsInput from 'react-tagsinput';
+import 'react-tagsinput/react-tagsinput.css'; // If using WebPack and style-loader.
+import { suggestOrgs } from '../data/actions/organizationsActions';
+import organizations from '../data/sample_data/organizations';
 
 interface DispatchProps {
-  editEvent: typeof editEvent,
+  editEvent: typeof editEvent;
+  suggestOrgs: typeof suggestOrgs;
 }
 
 //create your forceUpdate hook
 function useForceUpdate() {
   const [value, setValue] = useState(0); // integer state
-  return () => setValue(value => value + 1); // update the state to force render
+  return () => setValue((value) => value + 1); // update the state to force render
 }
 
 const EventModal = (props: Props): JSX.Element => {
@@ -31,35 +36,41 @@ const EventModal = (props: Props): JSX.Element => {
   const forceUpdate = useForceUpdate();
 
   const onClickViewDetails = () => {
-    props.setModalVisibility(false)
-  }
+    props.setModalVisibility(false);
+  };
 
   const onTagsChange = (newResources: Array<string>, changedResources: Array<Resource>) => {
-    let eventResponsibilities: Array<Responsibility> = event.responsiblities.map((resp) => resp)
-    event.responsiblities.length = 0
+    let eventResponsibilities: Array<Responsibility> = event.responsiblities.map((resp) => resp);
+    event.responsiblities.length = 0;
 
     newResources.forEach((resName: string) => {
       let found = false;
-      const res = stringToResource(resName)
+      const res = stringToResource(resName);
 
       eventResponsibilities.forEach((resp: Responsibility) => {
         if (resp.name == res) {
           found = true;
-          event.responsiblities.push(resp)
+          event.responsiblities.push(resp);
         }
-      })
+      });
 
       if (!found) {
         event.responsiblities.push({
           name: res,
-          status: 'unfilled'
-        })
+          status: 'unfilled',
+        });
       }
     })
 
     console.log(event)
     setEvent(event)
     props.editEvent(event)
+
+    props.suggestOrgs(
+      organizations[0],
+      event.location,
+      event.responsiblities.map((r: Responsibility) => r.name)
+    );
 
     forceUpdate()
   }
@@ -115,26 +126,23 @@ const EventModal = (props: Props): JSX.Element => {
           </div>
           <p>APCF was founded in 1990 by API community leaders in response to the need for alternative funding for Los Angeles-based nonprofit organizations serving API communities. Prior to its incorporation, less than 0.3% of all local foundation funds went to API agencies, according to a 1988 study by A3PCON (Asian Pacific Policy & Planning Council). In 1986, the United Way of Greater Los Angeles funded only five organizations serving APIs. To raise funds for community organizations serving API communities, APCF initiated employee giving campaigns at various workplaces, including private companies, federal agencies, state agencies, county agencies, city agencies, and nonprofit organizations. In the past decade, APCF has diversified its fundraising efforts by developing other avenues for giving. With the growth of donor-advised funds, giving circles, grant making, scholarship funds, and capacity building initiatives, APCF has been able to cultivate philanthropy among APIs while providing multiple vehicles for donors to support the community.</p>
 
-          <div className="event-modal-title-section">
-            <h2>Plan</h2>
+            {/* <div className="modal-footer"> */}
+            {/* <button type="button" className="organization-card-button" data-dismiss="modal">Close</button> */}
+            {/* <button type="button" className="organization-card-button">Save changes</button> */}
+            {/* </div> */}
           </div>
-          <p>{event.description}</p>
-
-          {/* <div className="modal-footer"> */}
-          {/* <button type="button" className="organization-card-button" data-dismiss="modal">Close</button> */}
-          {/* <button type="button" className="organization-card-button">Save changes</button> */}
-          {/* </div> */}
         </div>
       </div>
     </div>
-  </div>;
+  );
 };
 
 const mapDispatchToProps = (dispatch: Dispatch): DispatchProps => {
   return bindActionCreators(
     {
       // add other actions here
-      editEvent
+      editEvent,
+      suggestOrgs,
     },
     dispatch
   );
@@ -143,8 +151,8 @@ const mapDispatchToProps = (dispatch: Dispatch): DispatchProps => {
 const connector = connect(null, mapDispatchToProps);
 
 type Props = ConnectedProps<typeof connector> & {
-  setModalVisibility: (isOpen: boolean) => void,
-  event: Event,
+  setModalVisibility: (isOpen: boolean) => void;
+  event: Event;
 };
 
 export default connector(EventModal);
