310abde791326cc45b81ee6f9d1ab74479a0b16b
daniel-sm-yu <daniel.sm.yu@gmail.com>
2021-03-13 21:46:36 -0500

    add compatibilityScore to SampleComponent

--- src/components/SampleComponent.tsx
+++ src/components/SampleComponent.tsx
@@ -1,11 +1,11 @@
 /* Imports from packages */
-import { bindActionCreators, Dispatch } from "redux";
-import { connect } from "react-redux";
+import { bindActionCreators, Dispatch } from 'redux';
+import { connect } from 'react-redux';
 
-import { addOrg } from "../data/actions/organizationsActions";
-import Organization from "../data/types/organization";
-import RelationshipStatus from '../data/types/relationshipStatus'
-import { RootState } from "../data/reducers";
+import { addOrg } from '../data/actions/organizationsActions';
+import Organization from '../data/types/organization';
+import RelationshipStatus from '../data/types/relationshipStatus';
+import { RootState } from '../data/reducers';
 
 interface StateProps {
   organizations: Array<Organization>;
@@ -20,28 +20,23 @@ type Props = StateProps & DispatchProps;
 const SampleComponent = (props: Props): JSX.Element => {
   const onClick = () => {
     props.addOrg({
-      id: "4",
-      name: "Asian American Legal Foundation",
-      foundingYear: "2013",
-      address: "11 MALTA DR,SAN FRANCISCO , CA 94131",
-      state: "CA",
-      city: "SAN FRANCISCO",
-      county: "SAN FRANCISCO",
-      type: "Advocacy",
+      id: '4',
+      name: 'Asian American Legal Foundation',
+      foundingYear: '2013',
+      address: '11 MALTA DR,SAN FRANCISCO , CA 94131',
+      state: 'CA',
+      city: 'SAN FRANCISCO',
+      county: 'SAN FRANCISCO',
+      type: 'Advocacy',
       status: RelationshipStatus.noRelation,
-      image: "https://source.unsplash.com/Ch_QF4IocbU/150x150"
+      image: 'https://source.unsplash.com/Ch_QF4IocbU/150x150',
+      compatibilityScore: 0,
     });
   };
   return (
     <div className='sample-component'>
-      <p className='sample-component-text'>
-        {JSON.stringify(props.organizations)}
-      </p>
-      <button
-        type='button'
-        className='btn btn-outline-primary'
-        onClick={onClick}
-      >
+      <p className='sample-component-text'>{JSON.stringify(props.organizations)}</p>
+      <button type='button' className='btn btn-outline-primary' onClick={onClick}>
         Add Matched Org
       </button>
       <a href='/login'>Go to login</a>
@@ -65,12 +60,4 @@ const mapDispatchToProps = (dispatch: Dispatch): DispatchProps => {
   );
 };
 
-export default connect<
-  StateProps,
-  DispatchProps,
-  Record<string, unknown>,
-  RootState
->(
-  mapStateToProps,
-  mapDispatchToProps
-)(SampleComponent);
+export default connect<StateProps, DispatchProps, Record<string, unknown>, RootState>(mapStateToProps, mapDispatchToProps)(SampleComponent);
