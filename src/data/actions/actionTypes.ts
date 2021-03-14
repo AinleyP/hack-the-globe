96f9a41a55ea82cffe3f5492fa06587c047661be
daniel-sm-yu <daniel.sm.yu@gmail.com>
2021-03-13 21:46:57 -0500

    create SUGGEST_ORGS action

--- src/data/actions/actionTypes.ts
+++ src/data/actions/actionTypes.ts
@@ -6,7 +6,7 @@
 export const ADD_MATCHED_ORG = 'add_matched_org';
 export const ADD_SUGGESTED_ORG = 'add_suggested_org';
 export const ADD_ORG = 'add_org';
-
+export const SUGGEST_ORGS = 'suggest_orgs';
 
 export const SET_LOCATION = 'set_location';
 export const ADD_RESPONSIBLITY = 'add_responsiblity';
@@ -14,4 +14,3 @@ export const CLEAR_RESPONSIBILITIES = 'clear_responsibilities';
 
 export const ACCEPT_REQUEST_FROM_ORG = 'accept_request_from_org';
 export const SEND_REQUEST_TO_ORG = 'send_request_to_org';
-
